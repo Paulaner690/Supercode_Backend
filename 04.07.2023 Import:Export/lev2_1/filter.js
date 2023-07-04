02c7c2e32e396a85388b1df1c4ece32a48232c11
pPaulaner690 <paula.krug2000@gmail.com>
2023-07-04 17:03:55 +0200

    change lev2_1

--- 04.07.2023 Import:Export/lev2_1/filter.js
+++ 04.07.2023 Import:Export/lev2_1/filter.js
@@ -1,9 +1,13 @@
+import { data } from "./cities.js";
+
 // # Einwohner über 100.000 ausgeben
-export const morePopulation = (data) => {
+export const morePopulation = () => {
   return data.filter((city) => city.population > 100000);
 };
 
 // # Einwohner unter 100.000 ausgeben
-export const lessPopulation = (data) => {
+export const lessPopulation = () => {
   return data.filter((city) => city.population < 100000);
 };
+
+// oder Parameter "data" in den Funktionen übergeben und dann {data} nur in der main.js importieren
--- 04.07.2023 Import:Export/lev2_1/main.js
+++ 04.07.2023 Import:Export/lev2_1/main.js
@@ -1,6 +1,5 @@
-import { data } from "./cities.js";
 import { morePopulation } from "./filter.js";
 import { lessPopulation } from "./filter.js";
 
-console.log(morePopulation(data));
-console.log(lessPopulation(data));
+console.log(morePopulation());
+console.log(lessPopulation());
